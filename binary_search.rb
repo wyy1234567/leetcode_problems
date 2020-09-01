d1a24d14b489e70625d6a566a8c1f6e56421341f
Yueying <wangyy395@gmail.com>
2020-09-01 18:21:09 -0400

    binary search

--- binary_search.rb
+++ binary_search.rb
@@ -175,3 +175,39 @@ end
 
 print search_range([5,7,7,8,8,10], 8)
 print search_range([2, 2], 2)
+
+
+# Given a sorted array and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+# You may assume no duplicates in the array.
+
+# Example 1:
+# Input: [1,3,5,6], 5
+# Output: 2
+
+# Example 2:
+# Input: [1,3,5,6], 2
+# Output: 1
+
+def search_insert(nums, target)
+    start_ind = 0
+    last_ind = nums.size 
+
+    while start_ind + 1 < last_ind do
+        mid = start_ind + (last_ind - start_ind) / 2
+        if nums[mid] == target
+            return mid
+        end
+
+        if nums[mid] < target
+            start_ind = mid
+        else
+            last_ind = mid
+        end
+    end
+
+    if nums[start_ind] >= target
+        return start_ind
+    end
+
+    return last_ind
+end
