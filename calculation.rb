5ad37fd02f65fe75d954a2b76ebe0418b24d59bc
Yueying <wangyy395@gmail.com>
2020-09-05 17:50:47 -0400

    fibonacci number

--- calculation.rb
+++ calculation.rb
@@ -39,3 +39,26 @@ def add_binary(a, b)
     carry > 0 ? ans.prepend(carry.to_s) : ans
     ans
 end
+
+
+# You are climbing a stair case. It takes n steps to reach to the top.
+#Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+    
+#Example 1:
+#Input: 2
+#Output: 2
+#Explanation: There are two ways to climb to the top.
+#1. 1 step + 1 step
+#2. 2 steps
+
+#Fibonacci number
+def climb_stairs(n)
+    return 1 if n == 1
+    dp = [0] * (n + 1)
+    dp[1] = 1
+    dp[2] = 2
+    for i in 3..n do 
+        dp[i] = dp[i - 1] + dp[i - 2]
+    end
+    dp[n]
+end
