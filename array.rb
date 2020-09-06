37d8a011d7460e182f6df0c19a582592b1938bb7
Yueying <wangyy395@gmail.com>
2020-09-06 18:44:00 -0400

    array

--- array.rb
+++ array.rb
@@ -135,3 +135,28 @@ def generate(num_rows)
     end
     ans
 end
+
+
+# Say you have an array for which the ith element is the price of a given stock on day i.
+# If you were only permitted to complete at most one transaction (i.e., buy one and sell one share of the stock), design an algorithm to find the maximum profit.
+# Note that you cannot sell a stock before you buy one.
+
+# Example 1:
+# Input: [7,1,5,3,6,4]
+# Output: 5
+# Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+#              Not 7-1 = 6, as selling price needs to be larger than buying price.
+
+
+def max_profit(prices)
+    min_price = prices[0]
+    max_pro = 0 
+    price.each do |price|
+        if price <= min_price
+            min_price = price
+        elsif price - min_price > max_pro
+            max_pro = price - min_price
+        end
+    end
+    max_pro
+end
