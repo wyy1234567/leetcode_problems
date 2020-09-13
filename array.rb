f944aca627c4ed84c96bedb925688d8020dd762e
Yueying <wangyy395@gmail.com>
2020-09-13 00:33:27 -0400

    array moves

--- array.rb
+++ array.rb
@@ -208,3 +208,18 @@ def find_disappeared_numbers(nums)
     end
     ans
 end
+
+
+# Given a non-empty integer array of size n, find the minimum number of moves required to make all array elements equal, where a move is incrementing n - 1 elements by 1.
+# Example:
+# Input:
+# [1,2,3]
+# Output:
+# 3
+# Explanation:
+# Only three moves are needed (remember each move increments two elements):
+# [1,2,3]  =>  [2,3,3]  =>  [3,4,3]  =>  [4,4,4]
+
+def min_moves(nums)
+    nums.sum - nums.size * nums.min
+end
