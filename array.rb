8d975907f23da999b915a6b22775245bbfe7bc41
Yueying <wangyy395@gmail.com>
2020-09-14 17:41:10 -0400

    find pairs in array

--- array.rb
+++ array.rb
@@ -304,3 +304,33 @@ def find_relative_ranks(nums)
     end
     nums
 end
+
+# Given an array of integers and an integer k, you need to find the number of unique k-diff pairs in the array. Here a k-diff pair is defined as an integer pair (i, j), where i and j are both numbers in the array and their absolute difference is k.
+# Example 1:
+# Input: [3, 1, 4, 1, 5], k = 2
+# Output: 2
+# Explanation: There are two 2-diff pairs in the array, (1, 3) and (3, 5).
+# Although we have two 1s in the input, we should only return the number of unique pairs.
+
+def find_pairs(nums, k)
+    return 0 if k < 0
+    hash = {}
+    count = 0
+    nums.each do |num|
+        if hash[num]
+            if k == 0 && hash[num] == 1
+                count += 1
+            end
+            hash[num] += 1
+        else
+            if hash[num - k]
+                count += 1
+            end
+            if hash[num + k]
+                count += 1
+            end
+            hash[num] = 1
+        end
+    end
+    count
+end
