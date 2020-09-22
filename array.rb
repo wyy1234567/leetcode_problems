e57b7f2639f0ed0e027dab3ac03e2a1c74e05fe2
Yueying <wangyy395@gmail.com>
2020-09-22 18:34:12 -0400

    count triangles

--- array.rb
+++ array.rb
@@ -486,3 +486,33 @@ def two_sum(nums, target, curr, ans)
         end
     end
 end
+
+
+# Given an array consists of non-negative integers, your task is to count the number of triplets chosen from the array that can make triangles if we take them as side lengths of a triangle.
+# Example 1:
+# Input: [2,2,3,4]
+# Output: 3
+# Explanation:
+# Valid combinations are: 
+# 2,3,4 (using the first 2)
+# 2,3,4 (using the second 2)
+# 2,2,3
+
+def triangle_number(nums)
+    return 0 if !nums || nums.size < 3
+    nums = nums.sort
+    ans = 0
+    for i in 1...nums.size do 
+        left = 0
+        right = i - 1
+        while left < right do 
+            if nums[left] + nums[right] > nums[i]
+                ans += (right - left)
+                right -= 1
+            else
+                left += 1
+            end
+        end
+    end
+    ans
+end
