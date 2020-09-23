a79cab368d1a327de52751d9644991a6b66f4ea2
Yueying <wangyy395@gmail.com>
2020-09-23 17:45:48 -0400

    dp matrix

--- dynamic_pogramming.rb
+++ dynamic_pogramming.rb
@@ -22,3 +22,35 @@ def minimum_total(triangle)
 
     triangle[0][0]
 end
+
+# Given a m x n grid filled with non-negative numbers, find a path from top left to bottom right which minimizes the sum of all numbers along its path.
+# Note: You can only move either down or right at any point in time.
+# Example:
+# Input:
+# [
+#   [1,3,1],
+#   [1,5,1],
+#   [4,2,1]
+# ]
+# Output: 7
+# Explanation: Because the path 1→3→1→1→1 minimizes the sum
+
+def min_path_sum(grid)
+    return 0 if !grid || !grid[0]
+    return grid[0].last if grid.size < 1
+
+    for i in 0...grid.size do 
+        for j in 0...grid[i].size do 
+            if i == 0 && j == 0
+                grid[i][j] *= 1
+            elsif i == 0 && j != 0
+                grid[i][j] += grid[i][j - 1]
+            elsif j == 0 
+                grid[i][j] += grid[i - 1][j]
+            else
+                grid[i][j] += [grid[i][j - 1], grid[i - 1][j]].min
+            end
+        end
+    end
+    grid.last.last
+end
