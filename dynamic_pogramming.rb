28b6ee7579549c2409a014d5cabe8a37c841cb5f
Yueying <wangyy395@gmail.com>
2020-09-23 18:16:18 -0400

    dp unique path matrix

--- dynamic_pogramming.rb
+++ dynamic_pogramming.rb
@@ -54,3 +54,41 @@ def min_path_sum(grid)
     end
     grid.last.last
 end
+
+# A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
+# The robot can only move either down or right at any point in time. The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
+# How many possible unique paths are there?
+# Input: m = 3, n = 2
+# Output: 3
+# Explanation:
+# From the top-left corner, there are a total of 3 ways to reach the bottom-right corner:
+# 1. Right -> Down -> Down
+# 2. Down -> Down -> Right
+# 3. Down -> Right -> Down
+
+def unique_paths(m, n)
+    return 0 if m == 0 && n == 0
+    return 1 if m == 0 || n == 0
+
+    arr = [0] * n
+    matrix = []
+    m.times do 
+        matrix << arr
+    end
+
+    for i in 0...m do 
+        for j in 0...n do 
+            if i == 0 && j == 0
+                matrix[i][j] *= 1
+            elsif i == 0 && j != 0
+                matrix[i][j] = matrix[i][j - 1]
+            elsif j == 0
+                matrix[i][j] = matrix[i - 1][j]
+            else
+                matrix[i][j] = matrix[i - 1][j] + matrix[i][j - 1]
+            end
+        end
+    end
+
+    matrix[m-1][n-1]
+end
