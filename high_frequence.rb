9ab3b968a34dbc70f080f57440536d06c57ac27b
Yueying <wangyy395@gmail.com>
2020-09-24 15:21:37 -0400

    subsets

---
+++ high_frequence.rb
@@ -0,0 +1,23 @@
+# 78. Subsets
+# Given a set of distinct integers, nums, return all possible subsets (the power set).
+# Note: The solution set must not contain duplicate subsets.
+# Input: nums = [1,2,3]
+# Output:[[3],[1],[2],[1,2,3],[1,3],[2,3],[1,2],[]]
+
+def subsets(nums)
+    return nil if !nums
+    return [[]] if nums.size == 0
+    ans = []
+    subset = []
+    helper(nums, subset, ans, 0)
+    ans
+end
+
+def helper(nums, subset, ans, start_index)
+    ans << Array.new(subset)
+    for i in start_index...nums.size do 
+        subset << nums[i]
+        helper(nums, subset, ans, i + 1)
+        subset.pop
+    end
+end
