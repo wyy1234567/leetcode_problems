2fe981b6fdc879a71596e53decfe1ae4b7aeae53
Yueying <wangyy395@gmail.com>
2020-09-24 14:25:31 -0400

    dp array

--- dynamic_pogramming.rb
+++ dynamic_pogramming.rb
@@ -161,3 +161,46 @@ def max_envelopes(envelopes)
     end
     max
 end
+
+# Given a set of distinct positive integers, find the largest subset such that every pair (Si, Sj) of elements in this subset satisfies:
+# Si % Sj = 0 or Sj % Si = 0.
+# If there are multiple solutions, return any subset is fine.
+# Example 1:
+# Input: [1,2,3]
+# Output: [1,2] (of course, [1,3] will also be ok)
+# Example 2:
+# Input: [1,2,4,8]
+# Output: [1,2,4,8]
+
+def largest_divisible_subset(nums)
+    return [] if !nums
+    return nums if nums.size == 1
+    nums = nums.sort
+    dp = [0] * nums.size #record the max size before curr index
+    pre = [0] * nums.size #record pre index of curr index
+    size = 0
+    index = -1
+    ans = []
+    for i in 0...nums.size do 
+        pre[i] = -1
+        dp[i] = 1
+        
+        for j in 0...i do 
+            if nums[i] % nums[j] == 0 && dp[i] < 1 + dp[j]
+                dp[i] = dp[j] + 1
+                pre[i] = j
+            end
+        end
+        
+        if size <= dp[i]
+            size = dp[i]
+            index = i
+        end
+    end
+    
+    for i in 0...size do 
+        ans.unshift(nums[index]) 
+        index = pre[index]
+    end
+    ans
+end
