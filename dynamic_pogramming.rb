14b8075dc7f6f1d23bd467fb3a97e362baecb3e6
Yueying <wangyy395@gmail.com>
2020-09-24 13:59:55 -0400

    dp array

--- dynamic_pogramming.rb
+++ dynamic_pogramming.rb
@@ -134,3 +134,30 @@ def length_of_lis(nums)
     end
     max
 end
+
+# You have a number of envelopes with widths and heights given as a pair of integers (w, h). One envelope can fit into another if and only if both the width and height of one envelope is greater than the width and height of the other envelope.
+# What is the maximum number of envelopes can you Russian doll? (put one inside other)
+# Note:
+# Rotation is not allowed.
+# Example:
+# Input: [[5,4],[6,4],[6,7],[2,3]]
+# Output: 3 
+# Explanation: The maximum number of envelopes you can Russian doll is 3 ([2,3] => [5,4] => [6,7]).
+
+def max_envelopes(envelopes)
+    return 0 if !envelopes
+    return 1 if envelopes.size == 1
+    envelopes = envelopes.sort
+    max = 0
+    dp = [0] * envelopes.size
+    for i in 0...envelopes.size do 
+        dp[i] = 1
+        for j in 0...i do 
+            if envelopes[i][0] > envelopes[j][0] && envelopes[i][1] > envelopes[j][1]
+                dp[i] = dp[i] > dp[j] + 1 ? dp[i] : dp[j] + 1
+            end
+        end
+        max = [max, dp[i]].max
+    end
+    max
+end
