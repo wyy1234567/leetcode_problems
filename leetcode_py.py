45c57b271a0950ef0c8c8982cfdce1bcaddc3cee
Yueying <wangyy395@gmail.com>
2020-10-01 15:28:05 -0400

    py heap

--- leetcode_py.py
+++ leetcode_py.py
@@ -1,4 +1,5 @@
 import heapq
+import math
 
 def kClosest(points, K): 
     heap = []
@@ -76,3 +77,29 @@ class Node:
 
         return head.next
 
+# Given a list of scores of different students, return the average score of each student's top five scores in the order of each student's id.
+# Each entry items[i] has items[i][0] the student's id, and items[i][1] the student's score.  The average score is calculated using integer division.
+# Example 1:
+# Input: [[1,91],[1,92],[2,93],[2,97],[1,60],[2,77],[1,65],[1,87],[1,100],[2,100],[2,76]]
+# Output: [[1,87],[2,88]]
+# Explanation: 
+# The average of the student with id = 1 is 87.
+# The average of the student with id = 2 is 88.6. But with integer division their average converts to 88.
+
+def highFive(slist):
+    shash = {}
+    for (x, y) in slist:
+        if x not in shash:
+            shash[x] = [y] #value is a heap: [sum, avg]
+        else:
+            if len(shash[x]) == 5:
+                heapq.heappushpop(shash[x], y)
+            else:
+                heapq.heappush(shash[x], y)
+
+    print(shash)
+    res = []
+    for k in shash:
+        avg = math.floor(sum(shash[k]) / len(shash[k]))
+        res.append([k, avg])
+    return res
