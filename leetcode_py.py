74d712b842d3a59adfcd6f20c85c5495fe6fc2a3
Yueying <wangyy395@gmail.com>
2020-10-06 16:02:44 -0400

    merge intervals

--- leetcode_py.py
+++ leetcode_py.py
@@ -568,3 +568,14 @@ def atoi(s):
         
     return ans 
         
+
+# [[1,3],[2,6],[8,10],[15,18]] => [[1,6],[8,10],[15,18]]
+def mergeIntervals(intervals):
+    intervals.sort(key = lambda l:l[0])
+    ans = []
+    for i in intervals:
+        if not ans or i[0] > ans[-1][1]:
+            ans.append(i)
+        else:
+            ans[-1][1] = max(ans[-1][1], i[1])
+    return ans
