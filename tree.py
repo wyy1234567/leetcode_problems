36a4accc4d611ad61dc86c4d9705a80269e1c6d2
Yueying <wangyy395@gmail.com>
2020-10-14 14:24:23 -0400

    max path sum

--- tree.py
+++ tree.py
@@ -23,3 +23,28 @@ def maxSum(root):
     if not root:
         return -1
     return max(maxSum(root.left) + root.val, maxSum(root.right)+ root.val, root.val) 
+
+
+class Solution:
+    def maxPathSum(self, root):
+        self.maxSum = float('-inf')
+        self.pathSumRoot(root)
+        return self.maxSum
+    
+    
+    def pathSumRoot(self, root):
+        if not root:
+            return 0
+        
+        leftSum = self.pathSumRoot(root.left)
+        if leftSum < 0: 
+            leftSum = 0
+        
+        rightSum = self.pathSumRoot(root.right)
+        if rightSum < 0: 
+            rightSum = 0
+            
+        self.maxSum = max(self.maxSum, leftSum + rightSum + root.val)
+        
+        return max(leftSum + root.val, rightSum + root.val)
+        
