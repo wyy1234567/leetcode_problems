b31f62d49c0071441a01a72b96ee8a28f4f9461b
Yueying <wangyy395@gmail.com>
2020-10-14 14:20:06 -0400

    max path sum

--- tree.py
+++ tree.py
@@ -1,20 +1,25 @@
-from collections import defaultdict, deque
+# from collections import defaultdict, deque
 
-def vertcalTraversal(root):
-    from collections import defaultdict, deque
-    nhash = collections.defaultdict(list)
-    queue = collections.deque()
+# def vertcalTraversal(root):
+#     from collections import defaultdict, deque
+#     nhash = collections.defaultdict(list)
+#     queue = collections.deque()
 
-    queue.append((root, 0))
+#     queue.append((root, 0))
 
-    while queue:
-        node, index = queue.popleft()
-        if node:
-            nhash[index].append(node.val)
-            queue.append((node.left, index - 1))
-            queue.append((node.right, index + 1))
+#     while queue:
+#         node, index = queue.popleft()
+#         if node:
+#             nhash[index].append(node.val)
+#             queue.append((node.left, index - 1))
+#             queue.append((node.right, index + 1))
 
-    return [nhash[key] for key in sorted(nhash)]
+#     return [nhash[key] for key in sorted(nhash)]
 
 
 
+#Binary Tree Maximum Path Sum II
+def maxSum(root):
+    if not root:
+        return -1
+    return max(maxSum(root.left) + root.val, maxSum(root.right)+ root.val, root.val) 
