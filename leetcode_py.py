f1ef867c744309d77d6eedf84437d852683a8644
Yueying <wangyy395@gmail.com>
2020-10-15 17:48:56 -0400

    mountain arr

--- leetcode_py.py
+++ leetcode_py.py
@@ -719,3 +719,21 @@ def findWord(s, wordDict):
             partitions.append(word + ' ' + w)
     memo[s] = partitions
     return partitions
+
+
+def findPeak(nums):
+    left = 0 
+    right = len(nums) - 1
+        
+    while left + 1 < right:
+        mid = left + (right - left) // 2 
+        if nums[mid] > nums[mid - 1] and nums[mid] > nums[mid + 1]:
+            return mid 
+        elif nums[mid] > nums[mid - 1] and nums[mid] < nums[mid + 1]:
+            left = mid 
+        else:
+            right = mid 
+        
+    if nums[left] > nums[right]:
+        return left
+    return right
