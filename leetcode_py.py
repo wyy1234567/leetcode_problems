63fcce6ea6e871a4db46b8d6679ff533b3b5ac50
Yueying <wangyy395@gmail.com>
2020-10-29 17:47:04 -0400

    paint house

--- leetcode_py.py
+++ leetcode_py.py
@@ -1023,3 +1023,15 @@ def getFactors(n):
 
     dfs(n, 2, [])
     return result
+
+def painthouse(costs):
+    n = len(costs)
+    row = [0] * 3
+    matrix = [row for _ in range(n)]
+    matrix[0][0], matrix[0][1], matrix[0][2] = costs[0][0], costs[0][1], costs[0][2]
+    for i in range(1, n):
+        matrix[i][0] = costs[i][0] + min(matrix[i-1][1], matrix[i-1][2])
+        matrix[i][1] = costs[i][0] + min(matrix[i-1][0], matrix[i-1][2])
+        matrix[i][2] = costs[i][0] + min(matrix[i-1][0], matrix[i-1][1])
+
+    return min(matrix[n-1][0], matrix[n-1][1],matrix[n-1][2])
