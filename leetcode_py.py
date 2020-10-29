6a7b54ebd2ebd46d920897bffff1ce2c2121f512
Yueying <wangyy395@gmail.com>
2020-10-29 17:58:56 -0400

    reaching points

--- leetcode_py.py
+++ leetcode_py.py
@@ -1090,3 +1090,9 @@ def dailyTemperatures(T):
         stack.append(index)
     
     return result
+
+def reachingPoints(sx, sy, tx, ty):
+    while sx < tx and sy < ty:
+        tx, ty = tx % ty, ty % tx
+    
+    return sx == tx and sy <= ty and (ty - sy) % sx == 0 or sy == ty and sx <= tx and (tx - sx) % sy == 0
