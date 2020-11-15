7758f9310dfa080c32ddd5344624161f96887ba0
Yueying <wangyy395@gmail.com>
2020-11-15 16:04:26 -0500

    match

--- math.rb
+++ math.rb
@@ -175,3 +175,35 @@ def is_power_of_four(num)
     end
     return false
 end
+
+def is_match(s, p)
+    return false if !s || !p
+    column = [false] * (p.size + 1)
+    dp = []
+    (s.size + 1).times do 
+        dp << Array.new(column)
+    end
+    
+    dp[0][0] = true
+    for i in 1..p.size do 
+        if p[i - 1] == '*' 
+            dp[0][i] = dp[0][i-2]
+        end
+    end
+    
+    for i in 1..s.size do 
+        for j in 1..p.size do 
+            if p[j-1] == s[i-1] || p[j-1] == '.'
+                dp[i][j] = dp[i-1][j-1]
+            elsif p[j - 1] == '*'
+                if p[j - 2] == s[i - 1] || p[j - 2] == '.'
+                    dp[i][j] = dp[i-1][j] || dp[i][j - 2]
+                else
+                    dp[i][j] = dp[i][j - 2]
+                end
+            end
+        end
+    end
+    print dp
+    dp[s.size][p.size]
+end
