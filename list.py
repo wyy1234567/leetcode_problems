1caf1e25b3cd242cade336764713ea619fe52a55
Yueying <wangyy395@gmail.com>
2020-11-22 15:47:34 -0500

    merge lists

--- list.py
+++ list.py
@@ -47,6 +47,20 @@ class ListNode:
         curr.next = prev
         return curr
 
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        if not l1:
+            return l2
+        if not l2:
+            return l1
+        
+        if l1.val < l2.val:
+            l1.next = self.mergeTwoLists(l1.next, l2)
+            return l1
+        else:
+            l2.next = self.mergeTwoLists(l1, l2.next)
+            return l2
+            
+
         
 
 
