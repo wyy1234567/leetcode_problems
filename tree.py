af8859dfe7ff5eb6c3c69eed7cb23ba542efe730
Yueying <wangyy395@gmail.com>
2020-12-12 16:44:42 -0500

    merge trees

--- tree.py
+++ tree.py
@@ -99,3 +99,17 @@ class TreeNode:
             
         depth = max(left, right) + 1
         return depth
+
+    def mergeTrees(self, t1: TreeNode, t2: TreeNode) -> TreeNode:
+        if not t1 and not t2:
+            return None
+        if not t1:
+            return t2
+        if not t2:
+            return t1
+        t1.left = self.mergeTrees(t1.left, t2.left)
+        t1.right = self.mergeTrees(t1.right, t2.right)
+        
+        t1.val += t2.val 
+        
+        return t1
