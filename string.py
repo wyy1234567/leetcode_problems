3f7f41750a9d935e4ade1922b948337fb778b23c
Yueying <wangyy395@gmail.com>
2020-12-15 17:12:56 -0500

    string to list

---
+++ string.py
@@ -0,0 +1,16 @@
+def minRemoveToMakeValid(s: str) -> str:
+        s = list(s)
+        stack = []
+        
+        for i in range(len(s)):
+            if s[i] == '(':
+                stack.append(i)
+            elif s[i] == ')':
+                if stack:
+                    stack.pop()
+                else:
+                    s[i] = ''
+                    
+        while stack:
+            s[stack.pop()] = ''
+        return ''.join(s)
