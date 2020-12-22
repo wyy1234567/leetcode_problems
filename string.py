6b3a6ce5391119f43860dad660ba3def3ddfa53e
Yueying <wangyy395@gmail.com>
2020-12-22 10:10:36 -0500

    strStr

--- string.py
+++ string.py
@@ -105,3 +105,19 @@ def generateParenthesis(n):
 
     backtrack('', 0, 0)
     return result
+
+
+def strStr(haystack, needle):
+
+    if not needle:
+        return 0 
+
+
+    for i in range(len(haystack) - len(needle) + 1):
+        for j in range(len(needle)):
+            if haystack[i + j] != needle[j]:
+                break 
+            if j == len(needle) - 1:
+                return i 
+    
+    return -1
