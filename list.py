39d74a6c82d76c950002e204a05d0c0ccbb401a6
Yueying <wangyy395@gmail.com>
2020-12-27 17:04:22 -0500

    find peak num

--- list.py
+++ list.py
@@ -272,3 +272,21 @@ def searchInsert(nums, target):
             return left 
         
         return right
+
+
+def findMaxElement(nums):
+    left = 0 
+    right = len(nums) - 1
+        
+    while left + 1 < right:
+        mid = left + (right - left) // 2 
+        if nums[mid] > nums[mid - 1] and nums[mid] > nums[mid + 1]:
+            return mid 
+        elif nums[mid] > nums[mid - 1] and nums[mid] < nums[mid + 1]:
+            left = mid 
+        else:
+            right = mid 
+        
+    if nums[left] > nums[right]:
+        return left
+    return right
