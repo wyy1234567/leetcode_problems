7023fdab09d06720f35714f1f13e82981bdd43cc
Yueying <wangyy395@gmail.com>
2021-01-02 22:42:25 -0500

    rotate matrix

--- list.py
+++ list.py
@@ -340,3 +340,15 @@ def subarraySum(nums, k):
             numDic[sum] = 1
         
     return count
+
+
+def rotateMatrix(matrix):
+    n = len(matrix)
+        
+    for i in range(n // 2 + n % 2):
+        for j in range(n // 2):
+            temp = matrix[n - 1 - j][i]
+            matrix[n - 1 - j][i] = matrix[n - 1 - i][n - j - 1]
+            matrix[n - 1 - i][n - j - 1] = matrix[j][n - 1 -i]
+            matrix[j][n - 1 - i] = matrix[i][j]
+            matrix[i][j] = temp
