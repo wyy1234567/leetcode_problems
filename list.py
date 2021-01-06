7cc80868488fee012cb2fd01eb0362183da7f809
Yueying <wangyy395@gmail.com>
2021-01-06 17:39:12 -0500

    matrix with obstacles

--- list.py
+++ list.py
@@ -492,3 +492,27 @@ def findMinRotatedDuplicates(nums):
             else:
                 right -= 1 
     return min(nums[left], nums[right])
+
+def uniquePathsWithObstacles(obstacleGrid):
+    col, row = len(obstacleGrid), len(obstacleGrid[0])
+
+    if obstacleGrid[0][0] == 0:
+        return 0 
+    
+    obstacleGrid[0][0] = 1 
+
+    for i in range(1, col):
+        obstacleGrid[i][0] = int(obstacleGrid[i][0] == 0 and obstacleGrid[i-1][0] == 1)
+    
+    for j in range(1, row):
+        obstacleGrid[0][j] = int(obstacleGrid[0][j] == 0 and obstacleGrid[0][j-1] == 1)
+    
+    for i in range(1, col):
+        for j in range(1, row): 
+
+            if obstacleGrid[i][j] == 1:
+                obstacleGrid[i][j] = 0
+            else:
+                obstacleGrid[i][j] = obstacleGrid[i-1][j] + obstacleGrid[i][j-1]
+
+    return obstacleGrid[col-1][row-1]
