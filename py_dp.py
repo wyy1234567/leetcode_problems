79a0ad36867c3aff643eac4c5f09a9252f82971a
Yueying <wangyy395@gmail.com>
2021-01-15 16:17:53 -0500

    count square subarry in a matrix

--- py_dp.py
+++ py_dp.py
@@ -20,3 +20,19 @@ def maximalSquare(matrix):
                 dp[i][j] = 0
             res = max(res, dp[i][j] ** 2)           
     return res
+
+def countSquares(matrix):
+    row, col = len(matrix), len(matrix[0])
+    dp = [[0 for _ in range(col + 1)] for _ in range(row + 1)]
+    res = 0 
+        
+    for i in range(1, row + 1):
+        for j in range(1, col + 1):
+            if matrix[i-1][j-1] == 1:
+                dp[i][j] = min(dp[i-1][j], dp[i][j-1], dp[i-1][j-1]) + 1
+                res += dp[i][j]
+            else:
+                dp[i][j] = 0
+                    
+    return res
+        
