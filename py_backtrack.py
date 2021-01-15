f6537b6f73013e8d0ed952db8d045b6e6077dd7f
Yueying <wangyy395@gmail.com>
2021-01-15 18:17:09 -0500

    backtrack of permutation of a list

--- py_backtrack.py
+++ py_backtrack.py
@@ -27,3 +27,18 @@ def subset(nums):
 
     backtrack(nums, [], res)
     return res
+
+#permutation
+def permutation(nums):
+    res = []
+
+    def backtrack(nums, path, res):
+        if not nums:
+            res.append(path)
+            return 
+
+        for i in range(len(nums)):
+            backtrack(nums[:i]+nums[i+1:], path+[nums[i]], res)
+    
+    backtrack(nums, [], res)
+    return res 
