0de8977560a3e19b05e6f162ba0e2ca08f60219c
Yueying <wangyy395@gmail.com>
2021-01-20 17:45:24 -0500

    dp find sum ways for an array

--- py_dp.py
+++ py_dp.py
@@ -59,7 +59,6 @@ def coinChange(amount, coins):
 
 # Given a non-empty array nums containing only positive integers, find if the array can be partitioned into two subsets such that the sum of elements in both subsets is equal.       
 def canPartition(nums):
-
     numsSum = sum(nums)
 
     if numsSum % 2 == 1:
@@ -75,3 +74,25 @@ def canPartition(nums):
                 dp[i] = dp[i] or dp[i-num]
     
     return dp[numsSum]
+
+
+def findTargetSumWays(nums, S):
+    totalSum = sum(nums)
+    if(S not in range(-1 * totalSum, totalSum + 1) ): return 0
+    dp = [ [ 0 for j in range( totalSum*2 + 1 ) ] for i in range(len(nums))]
+        
+    ## BASE CASE ## FIRST ROW ##
+    dp[0][totalSum + nums[0]] += 1
+    dp[0][totalSum - nums[0]] += 1
+        
+    for i in range(1, len(nums)):
+        for j in range( totalSum*2 + 1 ):
+                
+            if( j - nums[i] >= 0 and dp[i-1][j-nums[i]] > 0 ):          # left side
+                dp[i][j] += dp[i-1][j-nums[i]]
+                
+            if( j + nums[i] <= totalSum*2 and dp[i-1][j+nums[i]] > 0 ): # right side
+                dp[i][j] += dp[i-1][j+nums[i]]
+        
+    return dp[-1][totalSum + S]
+        
