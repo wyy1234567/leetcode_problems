01726bae6c337c338c9ea1265aad3536d368253c
Yueying <wangyy395@gmail.com>
2021-01-24 18:39:35 -0500

    dp find increasing score team

--- py_dp.py
+++ py_dp.py
@@ -132,4 +132,21 @@ def lengthOfLIS(nums):
         
     return res
 
-        
+# Given two lists, scores and ages, where each scores[i] and ages[i] represents the score and age of the ith player, respectively, return the highest overall score of all possible basketball teams.
+def bestTeamScore(scores, ages):
+    n = len(scores)
+    players = [[a, s] for a, s in zip(ages, scores)]
+    players.sort(reverse = True)
+
+    ans = 0
+    dp = [0]*n
+
+    for i in range(n):
+        score = players[i][1]
+        dp[i] = score
+        for j in range(i):
+            if players[j][1] >= players[i][1]:
+                dp[i] = max(dp[i], dp[j] + score)
+        ans = max(ans, dp[i])
+
+    return ans
