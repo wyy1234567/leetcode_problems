34259f2cdce5bf7ed4a4225cd51d88dae5602386
Yueying <wangyy395@gmail.com>
2021-01-24 18:25:17 -0500

    dp find longest increasing subsequence

--- py_dp.py
+++ py_dp.py
@@ -120,4 +120,16 @@ def knapsack(values, weights, maxWeightConstraint):
     
     return dp[row-1][col-1]
 
+def lengthOfLIS(nums):
+    dp = [1] * len(nums)
+    res = 0 
+        
+    for i in range(len(nums)):
+        for j in range(i):
+            if nums[j] < nums[i]:
+                dp[i] = max(dp[i], dp[j] + 1)
+        res = max(res, dp[i])
+        
+    return res
+
         
