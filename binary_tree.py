e7c0e4bace348c138dd348a6493db503f38dbe96
Yueying <wangyy395@gmail.com>
2021-01-25 18:25:49 -0500

    recursive find tree max depth

---
+++ binary_tree.py
@@ -0,0 +1,15 @@
+class Node:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+    #find max depth of a tree
+    def maxDepth(self, root):
+        if not root:
+            return 0
+        
+        left = self.maxDepth(root.left)
+        right = self.maxDepth(root.right)
+        
+        return max(left, right)+1
