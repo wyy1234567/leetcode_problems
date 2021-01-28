9baec514462260620da0865b8b188109265797c3
Yueying <wangyy395@gmail.com>
2021-01-28 16:47:30 -0500

    dfs find diameter of a tree

--- binary_tree.py
+++ binary_tree.py
@@ -13,3 +13,20 @@ class Node:
         right = self.maxDepth(root.right)
         
         return max(left, right)+1
+
+    def diameterOfBinaryTree(self, root):
+        self.ans = 0
+        def height(root):
+            if not root:
+                return 0
+            
+            left = height(root.left)
+            right = height(root.right)
+            
+            self.ans = max(self.ans, left + right)
+            
+            return max(left, right)+1
+        height(root)
+        return self.ans
+        
+    
