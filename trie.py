ba38f657bf22aa03c662b62563517f2c5a2bdb9e
Yueying <wangyy395@gmail.com>
2021-02-02 17:27:16 -0500

    trie to search word in a board

--- trie.py
+++ trie.py
@@ -46,3 +46,35 @@ class Trie:
         return True
 
 
+def findWords(board, words):
+    trie = Trie()
+
+    for w in words:
+        trie.insert(w)
+    
+    node = trie.root 
+    res = []
+    
+    def helper(board, i, j, path, node, res):
+
+        if node.isLeaf:
+            res.append(path)
+            node.isLeaf = False
+        if i < 0 or i >= len(board) or j < 0 or j >= len(board[0]):
+            return 
+        tmp = board[i][j]
+        node = node.children[ord(tmp) - ord('a')]
+        if not node:
+            return 
+        board[i][j] = "#"
+        helper(board, node, i+1, j, path+tmp, res)
+        helper(board, node, i-1, j, path+tmp, res)
+        helper(board, node, i, j-1, path+tmp, res)
+        helper(board, node, i, j+1, path+tmp, res)
+        board[i][j] = tmp
+    
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            helper(board, i, j, "", node, res)
+
+    return res
