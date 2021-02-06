fc30fdf93599c51859b7ebe8868c968caf195ebf
Yueying <wangyy395@gmail.com>
2021-02-06 16:30:59 -0500

    graph sorting: course scheduleI

--- list.py
+++ list.py
@@ -882,5 +882,27 @@ def findOrder(numCourses, prerequisites):
     for i in seen:
         if not i:
             return []
-            
+
     return result
+
+def canFinish(numCourses, prerequisites):
+    graph = [[] for _ in range(numCourses)]
+    visit = [0 for _ in range(numCourses)]
+    for x, y in prerequisites:
+        graph[x].append(y)
+        
+    def dfs(i):
+        if visit[i] == -1:
+            return False
+        if visit[i] == 1:
+            return True
+        visit[i] = -1
+        for j in graph[i]:
+            if not dfs(j):
+                return False
+        visit[i] = 1
+        return True
+    for i in range(numCourses):
+        if not dfs(i):
+            return False
+    return True
