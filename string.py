acad822fd53fb9651c456851a1ba6fdf61f5848e
Yueying <wangyy395@gmail.com>
2021-02-10 17:41:29 -0500

    recursion to find pow(n,x)

--- string.py
+++ string.py
@@ -178,4 +178,16 @@ def decodeWays(s):
 
     return dp[-1]
 
-            
+
+def myPow(x, n):
+    def function(base = x, exponent = abs(n)):
+        if exponent == 0:
+            return 1
+        elif exponent % 2 == 0:
+            return function(base * base, exponent // 2)
+        else:
+            return base * function(base * base, (exponent - 1) // 2)
+
+    f = function()
+    
+    return float(f) if n >= 0 else 1/f
