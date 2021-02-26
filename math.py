63f0b64a55180bf571f801326a7ff0ede660978f
Yueying <wangyy395@gmail.com>
2021-02-25 23:13:55 -0500

    calculate pow(n,x) iteration approach

--- math.py
+++ math.py
@@ -10,3 +10,24 @@ def pow_recursion(x, n):
         return pow_recursion(x, n // 2) * pow_recursion(x, n // 2)
     else:
         return pow_recursion(x, n // 2) * pow_recursion(x, n // 2) * x
+
+
+def pow_iteration(x, n):
+    if n == 0 or x == 1:
+        return 1 
+    if n == 1:
+        return x 
+    result = 1 
+
+    if n < 0:
+        return 1 / (x * pow_iteration(x, -(n+1)))
+    
+    while n > 1:
+        if n % 2 == 1:
+            result = result * n 
+        
+        x = x * x 
+        n //= 2
+    result *=  x 
+
+    return result
