95e6b0017428dd9393e64d02ecda847c478d2ce4
Yueying <wangyy395@gmail.com>
2021-02-27 16:50:27 -0500

    dp find longest common subsequence

--- py_dp.py
+++ py_dp.py
@@ -251,3 +251,17 @@ def countSubstrings(s):
                 dp[i][j] = True
                 count += 1
     return count
+
+
+def longestCommonSubsequence(text1, text2):
+    rows, cols = len(text1), len(text2)
+    dp = [[0 for _ in range(cols + 1)] for _ in range(rows + 1)]
+    
+    for i in range(1, rows + 1):
+        for j in range(1, cols + 1):
+            if text1[i-1] == text2[j-1]:
+                dp[i][j] = dp[i-1][j-1] + 1 
+            else:
+                dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+                
+    return dp[rows][cols]
