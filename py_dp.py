009f588fdc139bdc67e3cd26c32c63129eaa28ae
Yueying <wangyy395@gmail.com>
2021-03-02 14:35:50 -0500

    dp find bst number

--- py_dp.py
+++ py_dp.py
@@ -281,3 +281,14 @@ def minDistance(word1: str, word2: str) -> int:
                 dp[i][j] = min(dp[i - 1][j], dp[i][j - 1]) + 1 
     
     return dp[rows][cols]
+
+
+#number of unique bst with input integer n
+def numTrees(n):
+    dp = [0] * (n+1)
+    dp[0] = dp[1] = 1 
+    for i in range(2, n+1):
+        for j in range(1, i+1):
+            dp[i] += dp[j-1] * dp[i-j]
+    return dp[n]
+        
