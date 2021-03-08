ee1a79ff57ac06f4850ea7dd66b579646ee60824
Yueying <wangyy395@gmail.com>
2021-03-08 17:09:29 -0500

    merge sort find the largest number

--- math.py
+++ math.py
@@ -61,3 +61,43 @@ def threeSum(nums):
         return result
     
     
+# Given a list of non-negative integers nums, arrange them such that they form the largest number.
+# Input: nums = [3,30,34,5,9]
+# Output: "9534330"
+def largestNumber(nums):
+    nums = merge_sort(nums, 0, len(nums)-1)
+    res = ''
+    for n in nums:
+        res += str(n)
+    
+    return str(int(res))
+
+def merge_sort(nums, left, right):
+    if left > right:
+        return 
+    if left == right:
+        return [nums[left]]
+    
+    mid = left + (right - left) // 2
+    left_half = merge_sort(nums, left, mid)
+    right_half = merge_sort(nums, mid + 1, right)
+
+    return merge(left_half, right_half)
+
+def compare(n1, n2):
+    return str(n1) + str(n2) > str(n2) + str(n1)
+
+def merge(l1, l2):
+    res, i, j = [], 0, 0
+
+    while i < len(l1) and j < len(l2):
+        if compare(l1[i], l2[j]):
+            res.append(l1[i])
+            i += 1
+        else:
+            res.append(l2[j])
+            j += 1 
+    
+    res.extend(l1[i:] or l2[j:])
+    return res
+
